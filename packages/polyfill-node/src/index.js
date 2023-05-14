106bd268a1a117979fa6cc1c4187a323d45adac8
bai.zixv <bai.zixv@foxmail.com>
2023-05-15 07:26:43 +0800

    chore：更名，打包文件名称

---
+++ packages/polyfill-node/src/index.js
@@ -0,0 +1,9 @@
+import { readFileAsync } from './node-fs'
+import { __filename, __dirname, transPath } from './node-path'
+
+export default {
+  readFileAsync,
+  transPath,
+  __filename,
+  __dirname,
+}
