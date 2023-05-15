e554ae65ca27ade7f2b27f0e77b727d008df5772
bai.zixv <bai.zixv@foxmail.com>
2023-05-15 23:06:29 +0800

    chore： 添加新的开发依赖项

--- packages/polyfill-node/rollup.config.js
+++ packages/polyfill-node/rollup.config.js
@@ -1,14 +1,156 @@
-import babel from 'rollup-plugin-babel'
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'lib/bundle.js',
-    format: 'umd',
-    name: '@baizixv/polyfill-node',
+import { defineConfig } from 'rollup'
+import babel from '@rollup/plugin-babel'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
+
+import pkg from './package.json'
+
+const extensions = ['.ts']
+// const noDeclarationFiles = { compilerOptions: { declaration: false } }
+
+const babelRuntimeVersion = pkg.dependencies['@babel/runtime'].replace(/^[^0-9]*/, '')
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+].map(name => RegExp(`^${name}($|/)`))
+
+export default defineConfig([
+  // CommonJS
+  {
+    input: 'src/index.js',
+    output: { file: 'dist/cjs/index.js', format: 'cjs', indent: false },
+    external,
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      babel({
+        extensions,
+        plugins: [
+          ['@babel/plugin-transform-runtime', { version: babelRuntimeVersion }],
+          ['./scripts/mangleErrors.js', { minify: false }],
+        ],
+        babelHelpers: 'runtime',
+      }),
+    ],
   },
-  plugins: [
-    babel({
-      exclude: ['node_modules/**'],
-    }),
-  ],
-}
+
+  // ES
+  {
+    input: 'src/index.ts',
+    output: { file: 'es/hera-core.js', format: 'es', indent: false },
+    external,
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        plugins: [
+          ['@babel/plugin-transform-runtime', { version: babelRuntimeVersion, useESModules: true }],
+        ],
+        babelHelpers: 'runtime',
+      }),
+    ],
+  },
+
+  // ES for Browsers
+  {
+    input: 'src/index.ts',
+    output: { file: 'es/hera-core.mjs', format: 'es', indent: false },
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      commonjs(),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        plugins: [['./scripts/mangleErrors.js', { minify: true }]],
+        skipPreflightCheck: true,
+        babelHelpers: 'bundled',
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+        },
+      }),
+    ],
+  },
+
+  // UMD Development
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/hera-core.js',
+      format: 'umd',
+      name: 'HeraCore',
+      indent: false,
+    },
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      commonjs(),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        plugins: [['./scripts/mangleErrors.js', { minify: false }]],
+        babelHelpers: 'bundled',
+      }),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      }),
+    ],
+  },
+
+  // UMD Production
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/hera-core.min.js',
+      format: 'umd',
+      name: 'HeraCore',
+      indent: false,
+    },
+    plugins: [
+      nodeResolve({
+        extensions,
+      }),
+      commonjs(),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        plugins: [['./scripts/mangleErrors.js', { minify: true }]],
+        skipPreflightCheck: true,
+        babelHelpers: 'bundled',
+      }),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+        },
+      }),
+    ],
+  },
+])
