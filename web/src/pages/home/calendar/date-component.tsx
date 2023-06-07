e8f240d38ed7b6f5c9d3d330580bbbc377c00083
bai.zixv <bai.zixv@foxmail.com>
2023-06-07 22:10:12 +0800

    update

--- web/src/pages/home/calendar/date-component.tsx
+++ web/src/pages/home/calendar/date-component.tsx
@@ -14,57 +14,54 @@ export const Clock = ({ currentTime }: { currentTime: number }) => {
 }
 
 // 农历日期组件
-export const LunarDate = React.memo(({ lunarDate }: { lunarDate: string }) => {
+export const LunarDate = ({ lunarDate }: { lunarDate: string }) => {
   return <p>农历: {lunarDate}</p>
-})
+}
 
 // 节气组件
-export const LunarJieQi = React.memo(({ jieQi }: { jieQi: string }) => {
+export const LunarJieQi = ({ jieQi }: { jieQi: string }) => {
   return <p>节气: {jieQi}</p>
-})
+}
 
 // 节日组件
-export const Festival = React.memo(({ festival }: { festival: string }) => {
+export const Festival = ({ festival }: { festival: string }) => {
   return <p>节日: {festival}</p>
-})
+}
 
 // 工作日
-export const DayType = React.memo(
-  ({ dateTypeStr }: { dateTypeStr: string }) => {
-    return <p>类型: {dateTypeStr}</p>
-  }
-)
+export const DayType = ({ dateTypeStr }: { dateTypeStr: string }) => {
+  return <p>类型: {dateTypeStr}</p>
+}
 
 // 天次
-export const DayOrder = React.memo(
-  ({ daysIndexStr }: { daysIndexStr: string }) => {
-    return <p>天次: {`${daysIndexStr}`}</p>
-  }
-)
-
+export const DayOrder = ({ daysIndexStr }: { daysIndexStr: string }) => {
+  return <p>天次: {`${daysIndexStr}`}</p>
+}
 // 周次
-export const WeekOrder = React.memo(({ weekOrder }: { weekOrder: string }) => {
+export const WeekOrder = ({ weekOrder }: { weekOrder: string }) => {
   return <p>周次: {weekOrder}</p>
-})
+}
 
 // 星座
-export const ZodiacSign = React.memo(({ zodiacSign }: { zodiacSign: string }) => {
+export const ZodiacSign = ({ zodiacSign }: { zodiacSign: string }) => {
   return <p>星座: {zodiacSign}</p>
-})
+}
 
 // 神煞宜项
-export const GodsGood = React.memo(({ good }: { good: string }) => {
+export const GodsGood = ({ good }: { good: string }) => {
   return <p>宜项: {good}</p>
-})
+}
 
 // 神煞禁忌
-export const GodsBad = React.memo(({ bad }: { bad: string }) => {
+export const GodsBad = ({ bad }: { bad: string }) => {
   return <p>禁忌: {bad}</p>
-})
+}
 
 // 阴历其它信息
-export const LunarOtherInfo = React.memo(
-  ({ lunarOtherInfo }: { lunarOtherInfo: string }) => {
-    return <p>卦象: {lunarOtherInfo}</p>
-  }
-)
+export const LunarOtherInfo = ({
+  lunarOtherInfo,
+}: {
+  lunarOtherInfo: string
+}) => {
+  return <p>卦象: {lunarOtherInfo}</p>
+}
