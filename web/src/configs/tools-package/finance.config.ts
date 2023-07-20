1bab684c907d72a891e91438a38edfbd6187a410
bai.zixv <bai.zixv@foxmail.com>
2023-07-21 04:48:59 +0800

    dev

--- web/src/configs/tools-package/finance.config.ts
+++ web/src/configs/tools-package/finance.config.ts
@@ -3,8 +3,8 @@ import { ToolCardConfig } from '@/typings/pages/webtools'
 
 export const financeListConfigs: ToolCardConfig[] = [
   {
-    name: '借贷利率计算',
-    description: '提供借贷利率计算，生成月供账单。或者根据月供推算实际利率.',
+    name: '借贷月供计算',
+    description: '提供借贷月供计算，生成月供账单。或者根据月供推算年化利率.',
     path: '/tools/money-price',
   },
 ]
