45fe9e1da431f60b8c1c55b16db80a0857f0c573
bai.zixv <bai.zixv@foxmail.com>
2023-07-22 03:40:38 +0800

    refactor: web, 函数生成pathNameMap，将配置集中在一处

--- web/src/configs/router.config/common.config.ts
+++ web/src/configs/router.config/common.config.ts
@@ -1,14 +1,30 @@
+import initialMenuConfigs from './menu.config'
+import { financeListConfigs } from './tools-package/finance.config'
+import { webtoolsListConfigs } from './webtools.config'
+
 // 面包屑导航，地址映射
-export const pathNameMap: Record<string, string> = {
-  '/': '主页',
-  '/home': '主页',
-  '/webtools': '常用在线工具',
-  '/webtools/qrcode': '二维码生成',
-  '/webtools/json_format': 'JSON格式化',
-  '/devtools': '开发者工具',
-  '/tools': '工具包',
-  '/tools/money-price': '借贷月供计算',
-  '/tools/convert-rate': '投资回报测算',
+export const getPathNameMap = () => {
+  const pathNameMap: Record<string, string> = {
+    '/': '主页',
+  }
+
+  const menuList = [...initialMenuConfigs]
+
+  menuList.forEach(menu => {
+    const { key, label } = menu
+    if (typeof label === 'string') {
+      pathNameMap[key] = label
+    }
+  })
+
+  const cardList = [...webtoolsListConfigs, ...financeListConfigs]
+
+  cardList.forEach(card => {
+    const { path, name } = card
+    pathNameMap[path] = name
+  })
+
+  return pathNameMap
 }
 
-
+export const pathNameMap = getPathNameMap()
